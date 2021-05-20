fdd56ccd8d19df58cfe02c3eec2d91556d315c6a
Taro Nakajima <nakajima.tarou@gmail.com>
2021-05-20 22:23:19 +0900

    Update SpecFileSplitter.py

--- SpecFileSplitter.py
+++ SpecFileSplitter.py
@@ -4,7 +4,7 @@ import os
 digits=4
 
 if(len(sys.argv)<2):
-	print("usage: python SpecReader_v1.py [datafile]")
+	print("usage: python SpecFileSplitter.py [datafile]")
 	sys.exit()
 
 if(os.path.exists(sys.argv[1])):
@@ -19,13 +19,13 @@ writeflag=0
 for line in FH:
 	if ('#L' in line):
 		filenum+=1
+	elif('#L' in line):
 		fn_str=str(filenum).zfill(digits)
 		FH2 = open("data_%s.txt"%(fn_str),"w")
 		writeflag=1
 		line2=line.replace('#L ','')
 		line3=line2.replace('Two Theta','TwoTheta')
 		FH2.write(line3)
-
 	else:
 		if('#' in line):
 			if(writeflag==1):
